4627e2905cd077c41218393e1982d0349d2a63b8
Louisii <louisimtd@outlook.com>
2024-04-08 14:56:04 -0300

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Louisi Dalazen on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
